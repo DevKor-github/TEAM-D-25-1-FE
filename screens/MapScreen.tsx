da3ce6b52792ed994e6eb55087eba66c01e8c26e
chsein <chltpdls4087@gmail.com>
2025-04-24 23:12:52 +0900

    chore: initial camera 세팅

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -4,7 +4,14 @@ import {View} from 'react-native';
 const MapScreen = () => {
   return (
     <View style={{flex: 1}}>
-      <NaverMapView style={{flex: 1}} />
+      <NaverMapView
+        style={{flex: 1}}
+        initialCamera={{
+          latitude: 37.586331295,
+          longitude: 127.029230599,
+          zoom: 10,
+        }}
+      />
     </View>
   );
 };
