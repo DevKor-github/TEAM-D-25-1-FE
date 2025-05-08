d04a6ce0cfa692e005c17127d14a60a1216a6b86
ujnimgnos <156194039+ujnimgnos@users.noreply.github.com>
2025-05-08 14:23:38 +0900

    feat: ios naver-api연동

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,19 +1,23 @@
-import {NaverMapView} from '@mj-studio/react-native-naver-map';
-import {View} from 'react-native';
+// screens/MapScreen.tsx
+import NaverMapView from 'react-native-nmap';
+import { View }      from 'react-native';
 
 const MapScreen = () => {
+  const initialLoction = {
+  latitude: 37.586331295,
+  longitude: 127.029230599,
+  zoom: 16,
+}
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
+      {/* 아래에 콘솔에 찍힌 그대로 정확히 복사한 Key를 넣어봅니다 */}
       <NaverMapView
-        style={{flex: 1}}
-        initialCamera={{
-          latitude: 37.586331295,
-          longitude: 127.029230599,
-          zoom: 10,
-        }}
+        style={{ flex: 1 }}
+        center={initialLoction}
       />
     </View>
   );
 };
 
 export default MapScreen;
+
