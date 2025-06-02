96b759bca09b56a50e503a9d4660eec30fcf9a52
chsein <chltpdls4087@gmail.com>
2025-06-02 16:03:30 +0900

    feat: add search bar on mapscreen

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -6,12 +6,14 @@ import {useEffect, useState} from 'react';
 import {
   Modal,
   Text,
+  TextInput,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import {getRestaruant} from '../apis/api/restaurant';
 import {Restaurant} from '../types/restaruant';
+import {SearchBar} from 'react-native-screens';
 
 const MapScreen = ({navigation}: {navigation: any}) => {
   // 식당 목록 불럭오기
@@ -20,6 +22,8 @@ const MapScreen = ({navigation}: {navigation: any}) => {
   // 선택한 식당 상태 저장
   const [selectedRestaurant, setSelectedRestaurant] =
     useState<Restaurant | null>(null);
+
+  // 모달 상태 저장장
   const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
@@ -30,9 +34,8 @@ const MapScreen = ({navigation}: {navigation: any}) => {
     fetchData();
   }, []);
 
-  const handleMarkerTap = (restaurant: Restaurant) => {
-    setSelectedRestaurant(restaurant);
-    setModalVisible(true);
+  const handleSearchClick = () => {
+    navigation.navigate('Login');
   };
 
   const handleCloseCustomModal = () => {
@@ -41,7 +44,37 @@ const MapScreen = ({navigation}: {navigation: any}) => {
   };
 
   return (
-    <View style={{flex: 1, width: '100%', height: '100%'}}>
+    <View style={{flex: 1}}>
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          //폰에 따라 달라야 할 것 같은데
+          top: 50,
+          left: 20,
+          right: 20,
+          backgroundColor: 'white',
+          borderRadius: 10,
+          paddingHorizontal: 15,
+          paddingVertical: 10,
+          zIndex: 1,
+          shadowColor: '#000',
+          shadowOffset: {width: 0, height: 2},
+          shadowOpacity: 0.2,
+          shadowRadius: 2,
+          elevation: 3,
+        }}
+        onPress={handleSearchClick}>
+        <TextInput
+          style={{
+            flex: 1,
+            fontSize: 16,
+            color: '#888', // placeholder 색상과 구분
+          }}
+          placeholder="어디로 가시나요?"
+          editable={false} // 텍스트 입력 방지
+          pointerEvents="none" // 텍스트 입력 필드 터치 방지 (TouchableOpacity가 터치되도록)
+        />
+      </TouchableOpacity>
       <NaverMapView
         style={{flex: 1}}
         initialCamera={{
