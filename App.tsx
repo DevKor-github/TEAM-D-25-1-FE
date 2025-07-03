e070b835773a8f67daff0a52486be610aef952a7
chsein <chltpdls4087@gmail.com>
2025-07-03 23:21:32 +0900

    .

--- App.tsx
+++ App.tsx
@@ -21,10 +21,13 @@ const App = () => {
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Password" component={PasswordScreen} /> 
+        <Stack.Screen name="Password" component={PasswordScreen} />
         <Stack.Screen name="Nickname" component={NicknameScreen} />
-        <Stack.Screen name="ProfileEdit" component={ProfileEditScreen}/>
-        <Stack.Screen name="KeywordSelection" component={KeywordSelectionScreen}/>
+        <Stack.Screen name="ProfileEdit" component={ProfileEditScreen} />
+        <Stack.Screen
+          name="KeywordSelection"
+          component={KeywordSelectionScreen}
+        />
         <Stack.Screen name="Map" component={BottomNavigationBar} />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
